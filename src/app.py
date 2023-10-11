87aed5e2735f47900247d56039f324f18fee9448
Me21cr <128864515+Me21cr@users.noreply.github.com>
2023-10-11 01:53:11 +0000

    finalizado

--- src/app.py
+++ src/app.py
@@ -1,14 +1,14 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
-import os
+import os, json
 from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Characters, Planets, Favorites
 #from models import Person
 
 app = Flask(__name__)
@@ -37,13 +37,122 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def handle_user():
+    users= User.query.all()
+    if len(users) < 1:
+        return jsonify({"msg": "not found"}), 404
+    serialized_users = list(map(lambda x: x.serialize(), users))
+    return serialized_users, 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/characters', methods=['GET'])
+def handle_characters():
+    characters= Characters.query.all()
+    if len(characters) < 1:
+        return jsonify({"msg": "not found"}), 404
+    serialized_characters = list(map(lambda x: x.serialize(), characters))
+    return serialized_characters, 200
 
-    return jsonify(response_body), 200
+@app.route('/planets', methods=['GET'])
+def handle_planets():
+   planets= Planets.query.all()
+   if len(planets) < 1:
+        return jsonify({"msg": "not found"}), 404
+   serialized_planets = list(map(lambda x: x.serialize(),planets))
+   return serialized_planets, 200
+
+@app.route('/favorites', methods=['GET'])
+def handle_favorites():
+   favorites= Favorites.query.all()
+   if len(favorites) < 1:
+        return jsonify({"msg": "not found"}), 404
+   serialized_favorites = list(map(lambda x: x.serialize(),favorites))
+   return serialized_favorites, 200
+
+@app.route('/user/<int:user_id>', methods=['GET'])
+def get_one_user(user_id):
+    user = User.query.get(user_id)
+    if user is None:
+        return jsonify({"msg": f"user with id {user_id} not found"}), 404
+    serialized_user = user.serialize()
+    return serialized_user, 200
+
+@app.route('/characters/<int:characters_id>', methods=['GET'])
+def get_one_characters(characters_id):
+    characters = Characters.query.get(characters_id)
+    if characters is None:
+        return jsonify({"msg": f"character with id {characters_id} not found"}), 404
+    serialized_characters =characters.serialize()
+    return serialized_characters, 200
+
+@app.route('/planets/<int:planets_id>', methods=['GET'])
+def get_one_planets(planets_id):
+    planets = Planets.query.get(planets_id)
+    if planets is None:
+        return jsonify({"msg": f"planets with id {planets_id} not found"}), 404
+    serialized_planets =planets.serialize()
+    return serialized_planets, 200
+
+@app.route('/user', methods=['POST'])
+def create_one_user():
+    body = json.loads(request.data)
+    new_user = User(
+        
+        email = body["email"],
+        password = body["password"],
+        is_active = True
+    )
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"msg": "user created succesfull"}), 200
+
+@app.route('/characters', methods=['POST'])
+def create_one_characters():
+    body = json.loads(request.data)
+    new_characters = Characters(
+        
+        name = body["name"],
+        gender= body["gender"],
+        eye_color= body["eye_color"],
+        hair_color= body["hair_color"],
+        )
+    db.session.add(new_characters)
+    db.session.commit()
+    return jsonify({"msg": "character created succesfull"}), 200
+
+@app.route('/planets', methods=['POST'])
+def create_one_planets():
+    body = json.loads(request.data)
+    new_planets = Planets(
+        
+        name = body["name"],
+        gravity= body["gravity"],
+        diameter= body["diameter"],
+        population= body["population"],
+        )
+    db.session.add(new_planets)
+    db.session.commit()
+    return jsonify({"msg": "planet created succesfull"}), 200
+
+@app.route('/favorite/planet/<int:planets_id>', methods=['DELETE'])
+def delete_one_favorite_planet(planets_id):
+    delete_favorite_planet = Favorites.query.get(planets_id)
+    db.session.delete(delete_favorite_planet)
+    db.session.commit()
+    return jsonify({"msg": "Favorite planet deleted succesfully"}), 200
+
+@app.route('/favorite/characters/<int:characters_id>', methods=['DELETE'])
+def delete_one_favorite_characters(characters_id):
+    delete_favorite_characters = Favorites.query.get(characters_id)
+    db.session.delete(delete_favorite_characters)
+    db.session.commit()
+    return jsonify({"msg": "Favorite character deleted succesfully"}), 200
+
+@app.route('/user/<int:user_id>', methods=['DELETE'])
+def delete_one_user(user_id):
+    delete_user = User.query.get(user_id)
+    db.session.delete(delete_user)
+    db.session.commit()
+    return jsonify({"msg": "user deleted succesfull"}), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -7,13 +7,74 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    favorites= db.relationship("Favorites", backref="user", lazy=True)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.id
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+
+class Characters(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    gender = db.Column(db.String(80), unique=False, nullable=False)
+    hair_color = db.Column(db.String(80), unique=False, nullable=False)
+    eye_color = db.Column(db.String(80), unique=False, nullable=False)
+    favorites= db.relationship("Favorites", backref="characters", lazy=True)
+   
+
+    def __repr__(self):
+        return '<Chararcters %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "gender": self.gender,
+            "hair_color": self.hair_color,
+            "eye_color": self.eye_color
+            # do not serialize the password, its a security breach
+        }
+    
+class Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    diameter = db.Column(db.String(80), unique=False, nullable=False)
+    gravity = db.Column(db.String(80), unique=False, nullable=False)
+    population = db.Column(db.String(80), unique=False, nullable=False)
+    favorites= db.relationship("Favorites", backref="planets", lazy=True)
+   
+
+    def __repr__(self):
+        return '<Planets %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "diameter": self.diameter,
+            "gravity": self.gravity,
+            "population": self.population
+            # do not serialize the password, its a security breach
+        }
+    
+class Favorites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    characters_id = db.Column(db.Integer, db.ForeignKey('characters.id'))
+    planets_id = db.Column(db.Integer, db.ForeignKey('planets.id'))
+    def __repr__(self):
+        return '<Favorites %r>' % self.id
+    def serialize(self):
+        return {
+            "id": self.id,
+            "planets_id": self.planets_id,
+            "user_id": self.user_id,
+            "characters_id": self.characters_id,
             # do not serialize the password, its a security breach
         }
